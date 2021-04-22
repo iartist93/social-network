3143ee6699f9860c83a2e8e25ca9fa0eab6b6ed2
Ahmad Ayman <mongez.me@gmail.com>
2021-04-23 00:43:28 +0200

    refined date formatting

--- src/features/posts/PostTimestamp.js
+++ src/features/posts/PostTimestamp.js
@@ -1,19 +1,27 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { formatDistance, formatRelative } from "date-fns";
+import {
+  formatDistance,
+  formatRelative,
+  parseISO,
+  formatDistanceToNow,
+  subDays
+} from "date-fns";
 
 const PostTimestamp = ({ postID, edited = false }) => {
   const post = useSelector((state) =>
     state.posts.list.find((post) => post.id === postID)
   );
 
-  const postDate = edited ? new Date(post.editedAt) : new Date(post.createdAt);
+  const postDate = edited ? parseISO(post.editedAt) : parseISO(post.createdAt);
   const currentDate = new Date();
   const postDateMS = postDate.getTime();
   const currentDateMS = currentDate.getTime();
   const dateDiff = Math.floor(
     (currentDateMS - postDateMS) / (1000 * 60 * 60 * 24)
   );
+
+  console.log(formatDistanceToNow(subDays(new Date(post.createdAt), 10)));
 
   return (
     <>
@@ -22,7 +30,9 @@ const PostTimestamp = ({ postID, edited = false }) => {
           ? formatDistance(postDate, new Date(), {
               addSuffix: true
             })
-          : formatRelative(postDate, new Date())}
+          : dateDiff < 7
+          ? formatRelative(postDate, new Date())
+          : formatDistanceToNow(postDate) + " ago"}
       </span>
     </>
   );
